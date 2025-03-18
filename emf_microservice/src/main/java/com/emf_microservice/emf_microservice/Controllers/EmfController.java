f1e2074264b69a89cadd613076b86f75b3ad0159
Glebas34 <147874028+Glebas34@users.noreply.github.com>
2025-03-18 11:58:57 +0000

    Added WMF processing

--- emf_microservice/src/main/java/com/emf_microservice/emf_microservice/Controllers/EmfController.java
+++ emf_microservice/src/main/java/com/emf_microservice/emf_microservice/Controllers/EmfController.java
@@ -8,14 +8,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.http.ResponseEntity;
 import org.apache.poi.hemf.usermodel.HemfPicture;
+import org.apache.poi.hwmf.usermodel.HwmfPicture;
 import org.apache.poi.util.Units;
 import java.awt.image.BufferedImage;
-import java.awt.geom.Dimension2D;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.geom.Rectangle2D;
+import java.awt.geom.Dimension2D;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import org.apache.commons.io.FilenameUtils;
 
 @RestController
 @RequestMapping("/emf")
@@ -23,36 +25,74 @@ public class EmfController {
 
     @PostMapping("/convert")
     public ResponseEntity<byte[]> convert(@RequestParam("file") MultipartFile file) {
-        try {  
-            HemfPicture emf = new HemfPicture(file.getInputStream());              
-            Dimension2D dim = emf.getSize();            
-            int width = Units.pointsToPixel(dim.getWidth());           
-            // keep aspect ratio for height        
-            int height = Units.pointsToPixel(dim.getHeight());             
-            double max = Math.max(width, height);          
-            if (max > 1500) {
-            width *= 1500/max;
-            height *= 1500/max;
+        try {
+            var fileExtension = FilenameUtils.getExtension(file.getOriginalFilename());
+            if (fileExtension != "emf" & fileExtension != "wmf")
+            {
+                return ResponseEntity.status(400).body(null);
             }
+            if (fileExtension == "emf") {
+                HemfPicture picture = new HemfPicture(file.getInputStream());              
+                Dimension2D dim = picture.getSize();            
+                int width = Units.pointsToPixel(dim.getWidth());           
+                // keep aspect ratio for height        
+                int height = Units.pointsToPixel(dim.getHeight());             
+                double max = Math.max(width, height);          
+                if (max > 1500) {
+                    width *= 1500/max;
+                    height *= 1500/max;
+                }
+                    
+                BufferedImage bufImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+                Graphics2D g = bufImg.createGraphics();
+                g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+                g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+                g.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+            
+                picture.draw(g, new Rectangle2D.Double(0,0,width,height));
                 
-            BufferedImage bufImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-            Graphics2D g = bufImg.createGraphics();
-            g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-            g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-            g.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
-        
-            emf.draw(g, new Rectangle2D.Double(0,0,width,height));
-            
-            g.dispose();
-            
-            ByteArrayOutputStream image = new ByteArrayOutputStream();
-            ImageIO.write(bufImg, "PNG", image);
-            image.flush();
-            byte[] imageBytes = image.toByteArray();
-            image.close();
+                g.dispose();
+                
+                ByteArrayOutputStream image = new ByteArrayOutputStream();
+                ImageIO.write(bufImg, "PNG", image);
+                image.flush();
+                byte[] imageBytes = image.toByteArray();
+                image.close();
 
-            return ResponseEntity.ok().body(imageBytes);
+                return ResponseEntity.ok().body(imageBytes);
+            }
+            else {
+                HwmfPicture picture = new HwmfPicture(file.getInputStream());              
+                Dimension2D dim = picture.getSize();            
+                int width = Units.pointsToPixel(dim.getWidth());           
+                // keep aspect ratio for height        
+                int height = Units.pointsToPixel(dim.getHeight());             
+                double max = Math.max(width, height);          
+                if (max > 1500) {
+                    width *= 1500/max;
+                    height *= 1500/max;
+                }
+                    
+                BufferedImage bufImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+                Graphics2D g = bufImg.createGraphics();
+                g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+                g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+                g.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+            
+                picture.draw(g, new Rectangle2D.Double(0,0,width,height));
+                
+                g.dispose();
+                
+                ByteArrayOutputStream image = new ByteArrayOutputStream();
+                ImageIO.write(bufImg, "PNG", image);
+                image.flush();
+                byte[] imageBytes = image.toByteArray();
+                image.close();
+
+                return ResponseEntity.ok().body(imageBytes);               
+            }
         } catch (IOException e) {
             return ResponseEntity.status(500).body(null);
         }
